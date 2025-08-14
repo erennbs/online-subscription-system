93c3604a27882287bf5991568a4eddf434b7af39
Eren <erenbas3453@gmail.com>
2025-08-14 20:22:06 +0300

    Fix auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
@@ -9,16 +8,5 @@ authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
 
 authRouter.post('/sign-out', signOut);
-=======
-import { signIn, signOut, signUp } from "../controllers/auth.controller";
-
-const authRouter = Router();
-
-authRouter.get('/sign-up', signUp);
-
-authRouter.get('/sign-in', signIn);
-
-authRouter.get('/sign-out', signOut);
->>>>>>> 54d10d84ffa7a00acc283cfbdddedcddba9a1d3f
 
 export default authRouter;
