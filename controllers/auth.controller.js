c2d55dd7ae1ee5e7dbf46ebc452e10fd39f84616
Eren <erenbas3453@gmail.com>
2025-08-14 20:42:01 +0300

    Implement sing-in controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,7 +46,40 @@ export const signUp = async (req, res, next) => {
 }
 
 export const signIn = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        
+        const user = await User.findOne({email: email});
+        
+        if (!user) {
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        
+        if (!isPasswordValid) {
+            const error = new Error("Wrong password");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        res.status(200).json({
+            succes: true,
+            message: "User signed in successfully",
+            data: {
+                token,
+                user
+            }
+        })
+
+
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const signOut = async (req, res, next) => {
