d797c36725fac320bda4aa08b704f5041f689e21
Eren <erenbas3453@gmail.com>
2025-08-15 17:36:29 +0300

    Add createSubscription and getUserSubscription controllers

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,37 +1,23 @@
 import { Router } from "express";
+import { createSubscription, getUserSubscription } from "../controllers/subscription.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => {
-    
-});
+subscriptionRouter.get('/', (req, res) => {});
 
-subscriptionRouter.get('/:id', (req, res) => {
-    
-});
+subscriptionRouter.get('/:id', (req, res) => {});
 
-subscriptionRouter.post('/', (req, res) => {
-    
-});
+subscriptionRouter.post('/', authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => {
-    
-});
+subscriptionRouter.put('/:id', (req, res) => {});
 
-subscriptionRouter.delete('/:id', (req, res) => {
-    
-});
+subscriptionRouter.delete('/:id', (req, res) => {});
 
-subscriptionRouter.get('/user/:id', (req, res) => {
-    
-});
+subscriptionRouter.get('/user/:id', authorize, getUserSubscription);
 
-subscriptionRouter.put('/:id/cancel', (req, res) => {
-    
-});
+subscriptionRouter.put('/:id/cancel', (req, res) => {});
 
-subscriptionRouter.get('/upcoming-renewals', (req, res) => {
-    
-});
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {});
 
 export default subscriptionRouter;
