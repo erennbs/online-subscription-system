459724e135600989a0aeba63b591a359bc479998
Eren <erenbas3453@gmail.com>
2025-08-19 12:48:58 +0300

    Integrate cancel subscription controller with Stripe and listen for related event

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -39,12 +39,16 @@ export const getUserSubscription = async (req, res, next) => {
 export const cancelSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.findOne({userId: req.user.id, status: "active"});
-
+        
         if (!subscription) {
             const error = new Error("No subscription found");
             error.statusCode = 404;
             throw error;
         }
+
+        await stripe.subscriptions.update(subscription.stripeSubscriptionId, {
+            cancel_at_period_end: true
+        })
 
         subscription.cancelAtPeriodEnd = true;
 
@@ -57,7 +61,6 @@ export const cancelSubscription = async (req, res, next) => {
 }
 
 export const createCheckoutSession = async (req, res, next) => {
-    const session = await mongoose.startSession();
     try {
         session.startTransaction()
         const plan = await SubscriptionPlan.findById(req.body.planId);
@@ -68,35 +71,11 @@ export const createCheckoutSession = async (req, res, next) => {
             throw error;
         }
         
-        let stripeCustomerId = req.user.stripeCustomerId
-
-        if (!stripeCustomerId) {
-            const customer = await stripe.customers.create({
-                name: req.user.name,
-                email: req.user.email,
-            });
-            stripeCustomerId = customer.id
-
-            await User.findByIdAndUpdate(req.user.id, {stripeCustomerId: stripeCustomerId});
-        }
-
-        const startDate = Date.now();
-        const end = new Date(startDate);
-        end.setMonth(end.getMonth() + (plan.interval == "month" ? 1 : 12));
-
-        const subscription = {
-            userId: req.user.id,
-            planId: req.body.planId,
-            status: "onhold",
-            currentPeriodStart: startDate,
-            currentPeriodEnd: end,
-            cancelAtPeriodEnd: false,
-        };
-
-        await Subscription.create(subscription);
+        const stripeCustomerId = req.user.stripeCustomerId
 
         const stripeSession = await stripe.checkout.sessions.create({
             mode: 'subscription',
+            payment_method_types: ['card'],
             customer: stripeCustomerId,
             line_items: [
                 {
@@ -109,12 +88,8 @@ export const createCheckoutSession = async (req, res, next) => {
             cancel_url: 'https://example.com/canceled.html',
         });
 
-        session.commitTransaction();
-        session.endSession();
         res.json( {data: stripeSession.url});
     } catch (error) {
-        session.abortTransaction();
-        session.endSession()
         next(error);
     }
 }
@@ -148,39 +123,45 @@ export const stripeWebhook = async (req, res, next) => {
         data = req.body.data;
         eventType = req.body.type;
     }
-
+    const user = await User.findOne({stripeCustomerId: data.object.customer});
+    
     switch (eventType) {
         case 'checkout.session.completed':
             // Payment is successful and the subscription is created
-            const sub = await Subscription.aggregate([{
-                $lookup: {
-                    from: "users",
-                    localField: "userId",
-                    foreignField: "_id",
-                    as: "user"
-                }
-            },
-            {
-                $match: {
-                    "user.stripeCustomerId": data.object.customer
-                }
-            }
-            ]).exec();
+            break;
+            case 'customer.subscription.created':
+                // Continue to provision the subscription as payments continue to be made.
+            const plan = await SubscriptionPlan.findOne({stripePriceId: data.object.plan.id});
+            
+            const startDate = Date.now();
+            const end = new Date(startDate);
+            end.setMonth(end.getMonth() + (plan.interval == "month" ? 1 : 12));
 
-            await Subscription.findByIdAndUpdate((sub[0]._id),
-            {"status": "active", "stripeSubscriptionId": data.object.subscription})
+            const subscription = {
+                userId: user._id,
+                planId: plan._id,
+                status: "active",
+                currentPeriodStart: startDate,
+                currentPeriodEnd: end,
+                cancelAtPeriodEnd: false,
+            };
+
+            await Subscription.create(subscription);
         break;
-        case 'invoice.paid':
-        // Continue to provision the subscription as payments continue to be made.
-        // Store the status in your database and check when a user accesses your service.
-        // This approach helps you avoid hitting rate limits.
-            console.log("invoice paid")
+        case 'customer.subscription.deleted':
+            console.log("Cancelled");
+            
+            const subscipritonToCancel = await Subscription.findOne({stripeSubscriptionId: data.object.id, status: "active"});
+            subscipritonToCancel.status = "canceled";
+            subscipritonToCancel.save();
         break;
         case 'invoice.payment_failed':
         // The payment failed or the customer does not have a valid payment method.
-        // The subscription becomes past_due. Notify your customer and send them to the
-        // customer portal to update their payment information.
-            console.log("payment failes");
+            const sub = await Subscription.findOne({userId: user._id});
+            if (sub) {
+                sub.status = "onhold";
+                sub.save();
+            }
         break;
         default:
         // Unhandled event type
